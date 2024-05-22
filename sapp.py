a14063bd08cafa8cd0b3ad06c5a0dc0b6c914bd9
georgegriff30 <georgegriffiths6@outlook.com>
2024-05-22 13:28:14 +0100

    new roberta model

--- sapp.py
+++ sapp.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 
 st.title('NER model')
-pipe = pipeline("token-classification", model="george6/NER", tokenizer=AutoTokenizer.from_pretrained("bert-base-uncased"))
+pipe = pipeline("token-classification", model="george6/roberta-finetuned-NER")
 x = st.text_input('Enter a custom message:', '')
 if x:
     list = []
